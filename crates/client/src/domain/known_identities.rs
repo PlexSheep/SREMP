3ff86bae569aab85b4606e94779df58f11a5f6c4
Christoph J. Scherr <software@cscherr.de>
2025-10-16 19:15:57 +0200

    feat(client): add create_or_update to known_identities #30

--- crates/client/src/domain/known_identities.rs
+++ crates/client/src/domain/known_identities.rs
@@ -1,26 +1,42 @@
 use std::{
-    collections::HashMap,
+    collections::{HashMap, hash_map::Entry},
     ops::{Deref, DerefMut},
 };
 
-use ed25519_dalek::VerifyingKey;
 use serde::{Deserialize, Serialize};
 
-use sremp_core::identity::ContactIdentity;
+use sremp_core::identity::{ContactId, ContactIdentity, Identity, Trust};
+
+use crate::error::ClientResult;
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize, Default)]
 pub struct KnownIdentities {
-    inner: HashMap<VerifyingKey, ContactIdentity>,
+    inner: HashMap<ContactId, ContactIdentity>,
 }
 
 impl KnownIdentities {
     pub fn new() -> Self {
         Self::default()
     }
+
+    pub fn create_or_update(&mut self, iden: &Identity) -> ClientResult<&ContactIdentity> {
+        let id = iden.id();
+        match self.inner.entry(id) {
+            Entry::Occupied(mut entry) => {
+                let contact = entry.get_mut();
+                contact.seen();
+            }
+            Entry::Vacant(entry) => {
+                let contact = ContactIdentity::from_peer_identity(iden.clone(), Trust::Unknown)?;
+                entry.insert(contact);
+            }
+        };
+        Ok(&self.inner[&id])
+    }
 }
 
 impl Deref for KnownIdentities {
-    type Target = HashMap<VerifyingKey, ContactIdentity>;
+    type Target = HashMap<ContactId, ContactIdentity>;
 
     fn deref(&self) -> &Self::Target {
         &self.inner
