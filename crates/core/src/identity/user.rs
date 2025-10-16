2fd3ab1325e2ced0e878a274a8c292ffa1e69b1b
Christoph J. Scherr <software@cscherr.de>
2025-10-16 18:15:36 +0200

    feat(core): expose access to user private keys and add noise key rotation to user identity #29

--- crates/core/src/identity/user.rs
+++ crates/core/src/identity/user.rs
@@ -3,7 +3,7 @@ use serde::{Deserialize, Serialize};
 use crate::{
     error::CoreResult,
     identity::{
-        Identity,
+        ContactId, Identity,
         crypto::{generate_good_key_ed25519, generate_good_key_x25519},
     },
 };
@@ -17,6 +17,7 @@ pub struct UserIdentity {
 
 impl UserIdentity {
     /// Creates a new [`UserIdentity`].
+    #[cold]
     pub fn create(username: &str) -> CoreResult<Self> {
         let mut identity_key = generate_good_key_ed25519();
         let noise_key = generate_good_key_x25519();
@@ -32,8 +33,23 @@ impl UserIdentity {
     }
 
     /// Returns a reference to the private key of this [`UserIdentity`].
-    pub fn private_key(&self) -> &ed25519_dalek::SigningKey {
+    #[inline(always)]
+    pub fn identity_private_key(&self) -> &ed25519_dalek::SigningKey {
         &self.identity_key
+    }
+
+    #[inline(always)]
+    pub fn noise_private_key(&self) -> &x25519_dalek::StaticSecret {
+        &self.noise_key
+    }
+
+    #[cold]
+    pub fn rotate_noise_private_key(&mut self) -> CoreResult<()> {
+        let noise_priv = generate_good_key_x25519();
+        let noise_pub = x25519_dalek::PublicKey::from(&noise_priv);
+
+        let mut id_priv = self.identity_private_key().clone();
+        self.identity.set_noise_key(noise_pub, &mut id_priv)
     }
 }
 
