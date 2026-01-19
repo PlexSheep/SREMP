17f73e9f03940704b82de5c231b5ab50a6872cfe
Christoph J. Scherr <software@cscherr.de>
2026-01-19 23:56:10 +0100

    test(client): connection established between forked tests #54

--- crates/client/tests/test_client_send_p2p.rs
+++ crates/client/tests/test_client_send_p2p.rs
@@ -1,23 +1,24 @@
 use std::{
+    io::Write,
     net::{IpAddr, Ipv4Addr, SocketAddr, SocketAddrV4},
     str::FromStr,
 };
 
+use env_logger::fmt::ConfigurableFormat;
+use log::info;
 use sremp_client::domain::{UiCommand, UiEvent};
+use sremp_core::identity::UserIdentity;
 
 use fork::{Fork, fork};
-use sremp_core::identity::UserIdentity;
+use ntest::timeout;
 
 fn wait(dur: u64) {
     let dur = std::time::Duration::from_mins(dur);
     std::thread::sleep(dur);
 }
 
-fn ack_evt(evt: UiEvent, prefix: Option<&str>) {
-    if let Some(prefix) = prefix {
-        print!("{prefix}: ")
-    }
-    println!("ACK UiEvent {evt}");
+fn ack_evt(evt: UiEvent) {
+    info!("TEST ACK UiEvent {evt}");
 }
 
 fn is_socket_bound_tcp(sock: &SocketAddr) -> bool {
@@ -49,64 +50,81 @@ fn start_client(
     (ui_command_tx, ui_event_rx)
 }
 
-// TODO: find a way to time this out! Sending and receiving a message over localhost should take
-// a fraction of a second at most.
+fn setup_logging(prefix: Option<&'static str>) {
+    let mut l = env_logger::builder();
+
+    l.filter_level(log::LevelFilter::Info);
+
+    let mut fmt = ConfigurableFormat::default();
+    if let Some(prefix) = prefix {
+        fmt.suffix(prefix);
+    }
+
+    l.parse_default_env()
+        .format(move |f, r| fmt.format(f, r))
+        .init();
+}
+
 // NOTE: This is the first time I'm doing automated testing for client functionality with fork().
 // The idea is that i have two processes that run my test code to talk over a loopback socket, but
 // i'm not sure if that actually works for tests like this. I guess i can call this an integration
 // test?
 #[test]
+#[timeout(500)]
 fn test_client_send_p2p() {
-    if let Err(err) = fork() {
-        panic!("{err}")
-    }
-    let mut rt = tokio::runtime::Runtime::new().expect("could not create tokio runtime");
-    let (ui_tx, ui_rx) = start_client(&mut rt);
-
     // surely nobody uses that specific port
     let lsock: SocketAddr = SocketAddr::from_str("127.0.0.1:31048").unwrap();
 
-    match fork() {
-        Ok(Fork::Parent(_)) => {
-            println!("P: Creating identity");
+    let role = fork().unwrap();
+
+    let mut rt = tokio::runtime::Runtime::new().expect("could not create tokio runtime");
+    let (ui_tx, ui_rx) = start_client(&mut rt);
+    match role {
+        Fork::Parent(_) => {
+            setup_logging(Some(" | P\n"));
             let iden = UserIdentity::create("parent").unwrap();
 
-            println!("P: Setting identity");
             ui_tx
                 .send_blocking(UiCommand::SetIdentity(Some(iden.into())))
                 .unwrap();
             // NOTE: set identity currently causes two events, the direct response and that the working copy was updated
-            ack_evt(ui_rx.recv_blocking().unwrap(), Some("P"));
-            ack_evt(ui_rx.recv_blocking().unwrap(), Some("P"));
+            ack_evt(ui_rx.recv_blocking().unwrap());
+            ack_evt(ui_rx.recv_blocking().unwrap());
 
-            println!("P: Starting listener");
             ui_tx
                 .send_blocking(UiCommand::StartListener(lsock))
                 .unwrap();
-            ack_evt(ui_rx.recv_blocking().unwrap(), Some("P"));
+            ack_evt(ui_rx.recv_blocking().unwrap());
+            wait(1); // assert will not work in many cases otherwise
 
             assert!(is_socket_bound_tcp(&lsock));
+
+            // TODO: assert that a connection is made
+            // TODO: check and accept identity
+            // TODO: message exchange
         }
-        Ok(Fork::Child) => {
+        Fork::Child => {
+            setup_logging(Some(" | C \n"));
             // NOTE: This runs as a test with cargo test. Cargo test does not care about the return
             // status of a child process, and why should it. But that means that an error here is
             // not necessarily treated as a failed test!
 
-            println!("C: Creating identity");
             let iden = UserIdentity::create("child").unwrap();
 
-            println!("C: Setting identity");
             ui_tx
                 .send_blocking(UiCommand::SetIdentity(Some(iden.into())))
                 .unwrap();
             // NOTE: set identity currently causes two events, the direct response and that the working copy was updated
-            ack_evt(ui_rx.recv_blocking().unwrap(), Some("P"));
-            ack_evt(ui_rx.recv_blocking().unwrap(), Some("P"));
+            ack_evt(ui_rx.recv_blocking().unwrap());
+            ack_evt(ui_rx.recv_blocking().unwrap());
 
-            println!("C: Connect to listener");
             ui_tx.send_blocking(UiCommand::Connect(lsock)).unwrap();
-            ack_evt(ui_rx.recv_blocking().unwrap(), Some("P"));
+            ack_evt(ui_rx.recv_blocking().unwrap());
+
+            // TODO: assert that a connection is made
+            // TODO: check and accept identity
+            // TODO: message exchange
         }
-        Err(err) => panic!("{err}"),
     }
+    wait(20);
 }
